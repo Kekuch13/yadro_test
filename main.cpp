523288d9f2d98fb09bbe639495654c3f34039913
Matvey Emelyanov <kassius2008@gmail.com>
2023-07-09 13:02:01 +0300

    обработка файла конфига

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@
 #include "TapeDevice.h"
 
 void sort(TapeDevice &inputTape, TapeDevice &outputTape, const int &RAM) {
-    std::vector<int> elements;
+    std::cout << "\nSorting started\n";
 
-    // храним временные ленты
-//    std::vector<TapeDevice> tapes;
+    std::vector<int> elements;
     std::vector<TapeDevice> tapes;
 
     while (!inputTape.eot()) {
@@ -23,7 +22,7 @@ void sort(TapeDevice &inputTape, TapeDevice &outputTape, const int &RAM) {
             inputTape.moveTapeRight();
         }
 
-        // сортируем только что считанные элементы и записываем их на временную ленту
+        // сортируем считанные элементы и записываем их на временную ленту
         // после очередного записанного элемента сдвигаем ленту на одну позицию вправо
         std::sort(elements.rbegin(), elements.rend());
         tapes.emplace_back("tmp\\" + std::to_string(tapes.size() + 1) + ".txt");
@@ -31,22 +30,22 @@ void sort(TapeDevice &inputTape, TapeDevice &outputTape, const int &RAM) {
             tapes.back().write(elem);
             tapes.back().moveTapeRight();
         }
-//        tapes.back().writeToFile();
-        tapes.back().moveTapeLeft(); // сдвигаем ленту на одну позицию влево, т.к. вышли за её пределы
+        tapes.back().moveTapeLeft(); // сдвигаем ленту на одну позицию влево, т.к. головка указывает на пустой элемент
 
-        // очищаем оперативную память
-        elements.resize(0);
+        elements.resize(0); // очищаем оперативную память
     }
+
+    std::cout << "Sorting in progress\n";
 
     int tmp, min;
     while (true) {
         min = INT_MAX;
         auto minIt = tapes.begin();
-        bool is_all_tapes = true;
+        bool end_of_all_tapes = true;
 
-        // Выбираем ленту с наименьшим элементом и запоминаем её
-        // Записываем полученное значение на выходную ленту
-        // Когда закончатся временные ленты, выходим из цикла
+        // выбираем ленту с наименьшим элементом
+        // записываем минимальное значение на выходную ленту
+        // когда закончатся временные ленты, выходим из цикла
         for (auto it = tapes.begin(); it != tapes.end(); ++it) {
             if (!it->eot()) {
                 tmp = it->read();
@@ -54,11 +53,10 @@ void sort(TapeDevice &inputTape, TapeDevice &outputTape, const int &RAM) {
                     min = tmp;
                     minIt = it;
                 }
-                is_all_tapes = false;
+                end_of_all_tapes = false;
             }
         }
-        if (is_all_tapes) {
-//            outputTape.writeToFile();
+        if (end_of_all_tapes) {
             break;
         } else {
             minIt->moveTapeLeft();
@@ -66,27 +64,34 @@ void sort(TapeDevice &inputTape, TapeDevice &outputTape, const int &RAM) {
             outputTape.moveTapeRight();
         }
     }
+    std::cout << "Sorting is finished\n";
 }
 
 int main() {
     try {
         std::string inputFile, outputFile, configFile;
         int RAM;
+        Config delays;
+
         std::cout << "Enter the path to the input tape file:";
         std::cin >> inputFile;
         std::cout << "Enter the path to the output tape file:";
         std::cin >> outputFile;
-        std::cout << "Enter the path to config file:";
+        std::cout << "Enter the path to Config file:";
         std:: cin >> configFile;
         std::cout << "Enter the amount of RAM:";
         std::cin >> RAM;
 
         boost::property_tree::ptree root;
-        boost::property_tree::read_json(configFile, root);
-        config delays;
-        delays.readDelay = root.get<int>("readDelay");
-        delays.writeDelay = root.get<int>("writeDelay");
-        delays.moveDelay = root.get<int>("moveDelay");
+        if (std::filesystem::exists(configFile)) {
+            boost::property_tree::read_json(configFile, root);
+        } else {
+            std::cerr << "\nCannot open Config file\n";
+        }
+
+        delays.readDelay = root.get_optional<int>("readDelay").get_value_or(0);
+        delays.writeDelay = root.get_optional<int>("writeDelay").get_value_or(0);
+        delays.moveDelay = root.get_optional<int>("moveDelay").get_value_or(0);
 
         TapeDevice inputTape(inputFile, delays);
         TapeDevice outputTape(outputFile, delays);
